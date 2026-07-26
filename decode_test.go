7753a0c098625b24e79802883853fd6cdbcb85be
Eiyarrr <eiyar@eiyar.com>
2026-07-26 04:09:47 -0700

    feat: added test for dictionary parsing

--- decode_test.go
+++ decode_test.go
@@ -25,3 +25,24 @@ func TestDecodeInteger(t *testing.T) {
 		t.Fatal("Wrong result")
 	}
 }
+
+func TestDecodeDictionary(t *testing.T) {
+	result, err := Decode([]byte("d3:cow3:moo4:spam4:eggse"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dict, ok := result.(map[string]any)
+	if !ok {
+		t.Fatal("Result is not a dictionary")
+	}
+
+	if dict["cow"] != "moo" {
+		t.Fatal("Wrong value for cow")
+	}
+
+	if dict["spam"] != "eggs" {
+		t.Fatal("Wrong value for spam")
+	}
+}
