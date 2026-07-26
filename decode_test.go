0dc54e9c1b8652adeb3181fa77467f1c63a2c0c9
Eiyarrr <eiyar@eiyar.com>
2026-07-25 23:54:10 -0700

    feat: added test case for '4:spam'

--- decode_test.go
+++ decode_test.go
@@ -1,3 +1,15 @@
-package bencode_test
+package bencode
 
-func TestDecodeString() { }
+import "testing"
+
+func TestDecodeString(t *testing.T) {
+	result, err := Decode([]byte("4:spam"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result != "spam" {
+		t.Fatal("Wrong result")
+	}
+}
