228e2070c16f5896cb8cb400ce36cdaf39fd7b5d
Eiyarrr <eiyar@eiyar.com>
2026-07-26 00:06:59 -0700

    feat: added string decoding

--- decode.go
+++ decode.go
@@ -1,3 +1,17 @@
 package bencode
 
-func Decode(data []byte) (any, error) { }
+func Decode(data []byte) (any, error) {
+	strLen := 0
+	str := ""
+	for _, b := range data {
+		// between 0 and 9
+		if b >= 48 && b <= 57 {
+			// normalize byte to 0
+			strLen += int(b) - 48
+			// colon ':'
+		} else if b != 58 {
+			str += string(b)
+		}
+	}
+	return str, nil
+}
